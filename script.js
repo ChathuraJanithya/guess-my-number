cfab0aaecf68f5f6ddb0a7d02b2144d00c4c8323
Chathura_Janithya <it21263880@my.sliit.lk>
2022-12-26 23:47:44 +0530

    final

--- script.js
+++ script.js
@@ -80,10 +80,10 @@ document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   displayMessage("Start guessing...");
-  documwt.querySelector(".score").textContent = score;
+  document.querySelector(".score").textContent = score;
   document.querySelector(".number").textContent = "?";
   document.querySelector(".guess").value = " ";
   document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.backgroundColor = "#eee";
   document.querySelector(".number").style.width = "15rem";
-  document.querySelector(".number").style.backgroundColor = "#222";
 });
