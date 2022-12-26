aeecf092ed9479fb2353109ff47c593767ce891c
Chathura_Janithya <it21263880@my.sliit.lk>
2022-12-26 21:32:28 +0530

    st3

--- script.js
+++ script.js
@@ -12,29 +12,45 @@ document.querySelector(".score").textContent = 20;
 
 document.querySelector(".guess").value = 23; */
 
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 document.querySelector(".check").addEventListener("click", function () {
-  const number = document.querySelector(".guess").value;
+  const number = Number(document.querySelector(".guess").value);
   console.log(secretNumber);
-
   let score = 20;
-
+  let highscore = 0;
   if (!number) {
-    document.querySelector(".message").textContent = "Not number!";
-  } else {
-    if (number == secretNumber) {
-      document.querySelector(".message").textContent = "Correct Number!";
-      document.querySelector(".number").textContent = secretNumber;
-      document.querySelector("body").style.backgroundColor = "#60b347";
-      document.querySelector(".number").style.width = "30rem";
-    } else if (number > secretNumber) {
-      document.querySelector(".message").textContent = "Too high!";
+    displayMessage("No number!");
+  } else if (number === secretNumber) {
+    displayMessage("Correct Number!");
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+  } else if (number > secretNumber) {
+    if (score > 1) {
+      displayMessage("Too high!");
       score--;
-    } else if (number < secretNumber) {
-      document.querySelector(".message").textContent = "Too low!";
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("💥 You lost the game!");
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (number < secretNumber) {
+    if (score > 1) {
+      displayMessage("Too low!");
       score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("💥 You lost the game!");
+      document.querySelector(".score").textContent = 0;
     }
   }
-  document.querySelector(".highscore").textContent = score;
 });
